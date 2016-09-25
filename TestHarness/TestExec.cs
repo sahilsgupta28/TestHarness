6e150fd16aafb570a09241721c143dd5f7390277
sahilgupta <cooldeveloper28@gmail.com>
2016-09-24 20:58:07 -0400

    Added XML Parser
    Added XML parser to parse test request
    Added sample xml file
    added

--- TestHarness/TestExec.cs
+++ TestHarness/TestExec.cs
@@ -2,6 +2,9 @@
  * Test Executive
  * Entry point for Test Harness
  * 
+ * Usage
+ *      TestHarness.exe repository_path testrequest1_path [(...)]
+ *      
  * FileName     : TestExec.cs
  * Author       : Sahil Gupta
  * Date         : 24 September 2016 
@@ -9,6 +12,7 @@
  */
 
 using System;
+using System.IO;
 
 namespace TestHarness
 {
@@ -16,6 +20,47 @@ namespace TestHarness
     {
         static void Main(string[] args)
         {
+            /*  Validate input
+             *  Checks if file exists 
+             */
+            if (0 == args.Length)
+            {
+                Console.WriteLine("Enter Repository path");
+                return;
+            }
+
+            if (!Directory.Exists(args[0]))
+            {
+                Console.WriteLine("Invalid Path {0}", args[0]);
+                return;
+            }
+
+            Console.WriteLine("Repository Path : {0}", args[0]);
+
+            /* @todo En-queue Test Request
+             */
+
+            /* Create AppDomain Manager
+             *  - @todo De-queues Test Request
+             *  - Creates AppDomain to process TestRequest
+             */
+            AppDomainMgr appDomainMgr = new AppDomainMgr();
+
+            appDomainMgr.RespoitoryPath = args[0];
+
+            if (args.Length > 1)
+            {
+                string[] TestReq = new string[args.Length - 1];
+
+                for (int i = 0; i < args.Length - 1; i++)
+                {
+                    TestReq[i] = args[i + 1];
+                }
+
+                appDomainMgr.ProcessTestRequests(TestReq);
+            }
+
+
         }
     }
 }
---
+++ TestHarness/XmlParser.cs
@@ -0,0 +1,120 @@
+﻿/**
+ * XML Parser
+ * Parses Test Request implemented as XML file to extract test driver & library
+ * 
+ * FileName     : XmlParser.cs
+ * Author       : Sahil Gupta
+ * Date         : 24 September 2016 
+ * Version      : 1.0
+ */
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using System.Collections.Generic;
+
+namespace TestHarness
+{
+    public class TestCaseData
+    {
+        public int Version { get; set; }
+        public string TestName { get; set; }
+        public string Author { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string TestDriver { get; set; }
+        public List<string> TestCode { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine("{0,12} : {1}", "Version", Version);
+            Console.WriteLine("{0,12} : {1}", "Author", Author);
+            Console.WriteLine("{0,12} : {1}", "TimeStamp", TimeStamp);
+            Console.WriteLine("{0,12} : {1}", "TestName", TestName);
+            Console.WriteLine("{0,12} : {1}", "TestDriver", TestDriver);
+            foreach (string Library  in TestCode)
+            {
+                Console.WriteLine("{0,12} : {1}", "Library", Library);
+            }
+            Console.WriteLine("");
+        }
+    }
+
+    public class XmlParser
+    {
+        private XDocument xDoc;
+        private List<TestCaseData> TestCase;
+
+        public XmlParser()
+        {
+            xDoc = new XDocument();
+            TestCase = new List<TestCaseData>();
+        }
+
+        public bool ParseTestRequest(string sTestRequest)
+        {
+            FileStream XML = null;
+            try
+            {
+                XML = new FileStream(sTestRequest, System.IO.FileMode.Open);
+                xDoc = XDocument.Load(XML);
+                if (xDoc == null)
+                {
+                    Console.WriteLine("Error: XDocument.Load({0})... FAILED.", XML);
+                    return false;
+                }
+
+                string Version = xDoc.Descendants("Version").First().Value;
+                string Author = xDoc.Descendants("Author").First().Value;
+                XElement[] xtests = xDoc.Descendants("Test").ToArray();
+
+                int numTests = xtests.Count();
+                for (int i = 0; i < numTests; i++)
+                {
+                    TestCaseData TestData = new TestCaseData();
+
+                    /* Fill in test case data */
+                    TestData.Version = Int32.Parse(Version);
+                    TestData.Author = Author;
+                    TestData.TimeStamp = DateTime.Now;
+
+                    TestData.TestName = xtests[i].Attribute("Name").Value;
+                    TestData.TestDriver = xtests[i].Element("TestDriver").Value;
+
+                    TestData.TestCode = new List<string>();
+                    IEnumerable<XElement> xTestCode = xtests[i].Elements("Library");
+                    foreach (var library in xTestCode)
+                    {
+                        TestData.TestCode.Add(library.Value);
+                    }
+
+                    /* Add this test data to list of test cases */
+                    TestCase.Add(TestData);
+                }
+
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine("Exception : {0}", Ex.Message);
+            }
+            finally
+            {
+                if (null != XML)
+                {
+                    XML.Close();
+                }
+                
+            }
+            return true;
+        }
+
+        public void DisplayTestRequest()
+        {
+            foreach (TestCaseData TestData in TestCase)
+            {
+                TestData.Display();
+            }
+        }
+
+    }
+}
