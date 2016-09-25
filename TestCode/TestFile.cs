46deab48400737c355c1ae1b1b95f752e67ed733
sahilgupta <cooldeveloper28@gmail.com>
2016-09-24 21:28:09 -0400

    Adding Test Drivers
    Added ITest Interface
    Added demo test code and test driver

---
+++ TestCode/TestFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TestCode
+{
+    public class TestCode
+    {
+        int data;
+
+        public TestCode()
+        {
+            data = 0;
+        }
+
+        public bool IsNonZero(int i)
+        {
+            data = i;
+
+            return (0 != data) ? true : false;
+        }
+
+        public void Display(string msg)
+        {
+            Console.WriteLine("TestCode::Display({0})", msg);
+        }
+
+        static void Main(string[] args)
+        {
+            TestCode tc = new TestCode();
+
+            tc.Display("HelloWorld");
+            Console.WriteLine("tc.IsNonZero(5) : {0}", tc.IsNonZero(5) ? "TRUE" : "FALSE");
+            Console.WriteLine("tc.IsNonZero(0) : {0}", tc.IsNonZero(0) ? "TRUE" : "FALSE");
+            
+        }
+    }
+}
---
+++ TestCode_TestDriver/TestDriver.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace TestDrivers 
+{
+    using TestInterface;
+    using TestCode;
+
+    public class TestDriver : ITest
+    {
+        TestCode demoTestCode;
+
+        public TestDriver()
+        {
+            demoTestCode = new TestCode();
+        }
+
+        public static ITest create()
+        {
+            return new TestDriver();
+        }
+
+        public bool test()
+        {
+            // Call Test Function 1
+            demoTestCode.Display("Test 1");
+            
+            // Call Test Function 2
+            if (false == demoTestCode.IsNonZero(5))
+            {
+                // return false if a test fails
+                return false;
+            }
+
+            //
+            // Call additional test functions
+            //
+            
+            // Return true if all tests pass.
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Local test");
+
+            ITest test = TestDriver.create();
+
+            if (test.test() == true)
+            {
+                Console.WriteLine("Test passed");
+            }
+            else
+            {
+                Console.WriteLine("Test failed");
+            }
+        }
+    }
+}
