e43f198f5b5a4d9378984628ea025d3bf0168ea3
sahilgupta <cooldeveloper28@gmail.com>
2016-09-24 20:18:34 -0400

    Test Harness Baseline
    commiting project files

---
+++ TestHarness/TestExec.cs
@@ -0,0 +1,21 @@
+﻿/**
+ * Test Executive
+ * Entry point for Test Harness
+ * 
+ * FileName     : TestExec.cs
+ * Author       : Sahil Gupta
+ * Date         : 24 September 2016 
+ * Version      : 1.0
+ */
+
+using System;
+
+namespace TestHarness
+{
+    class TestExec
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
