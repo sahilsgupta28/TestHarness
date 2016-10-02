9ba868c268482a8705633807eae4cbb6acf83935
sahilgupta <cooldeveloper28@gmail.com>
2016-10-02 14:28:29 -0400

    adding Logger to store test results

--- TestHarness/AppDomainMgr.cs
+++ TestHarness/AppDomainMgr.cs
@@ -27,6 +27,7 @@ namespace TestHarness
         /**********************************************************************
                                  M E M B E R S
          **********************************************************************/
+        FileMgr Database;
 
         public List<TestData> TestDrivers;
 
@@ -39,6 +40,7 @@ namespace TestHarness
         public AppDomainMgr(string path)
         {
             RepositoryPath = path;
+            Database = new FileMgr(path + "\\Log.txt");
         }
 
         public bool ProcessTestRequest(string sTestRequest)
@@ -97,9 +99,12 @@ namespace TestHarness
 
                 foreach (TestData td in TestDrivers)
                 {
+                    bool TestStatus;
+
                     Console.WriteLine("Testing ({0})", td.Name);
                     Console.WriteLine("--------------------------");
-                    if (td.TestDriver.test() == true)
+                    TestStatus = td.TestDriver.test();
+                    if (TestStatus == true)
                     {
                         Console.WriteLine("--------------------------");
                         Console.WriteLine("#### Test Status : PASS ####\n");
@@ -109,7 +114,10 @@ namespace TestHarness
                         Console.WriteLine("--------------------------");
                         Console.WriteLine("#### Test Status : FAIL ####\n");
                     }
+
                     Console.WriteLine("{0}", td.TestDriver.getLog());
+
+                    Database.WriteLog(td.Name, td.TestDriver.getLog(), TestStatus?"PASS":"FAIL");
                 }
             }
             catch (Exception Ex)
---
+++ TestHarness/FileMgr.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+namespace TestHarness
+{
+    class FileMgr
+    {
+        string filepath;
+
+        public FileMgr(string path)
+        {
+            filepath = path;
+        }
+
+        public void WriteLog(string LogMsg)
+        {
+            // The using statement automatically flushes AND CLOSES the stream and calls 
+            // IDisposable.Dispose on the stream object.
+            using (StreamWriter w = new StreamWriter(filepath, true))
+            {
+                w.WriteLine("{0} {1}\t{2}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString(), LogMsg);
+            }
+        }
+
+        public void WriteLog(string DriverName, string TestLog, string TestStatus)
+        {
+            // The using statement automatically flushes AND CLOSES the stream and calls 
+            // IDisposable.Dispose on the stream object.
+            using (StreamWriter w = new StreamWriter(filepath, true))
+            {
+                w.WriteLine("{0}.StartLog", DriverName);
+                w.WriteLine("DriverName : {0}", DriverName);
+                w.WriteLine("Test Status : {0}", TestStatus);
+                w.WriteLine("TimeStamp  : {0}\t{1}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
+                w.WriteLine("\n==================T.E.S.T  L.O.G.===================");
+                w.WriteLine("{0}", TestLog);
+                w.WriteLine("==========================================================");
+                w.WriteLine("{0}.EndLog", DriverName);
+            }
+        }
+
+        public void DisplayLog()
+        {
+            using (StreamReader sr = new StreamReader(filepath))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+            }
+        }
+
+        static void FileMgrMain(string[] args)
+        {
+            try
+            {
+                string path = @"c:\Log.txt";
+                FileMgr Logger = new FileMgr(path);
+
+                Logger.WriteLog("Test Log Msg");
+
+                Logger.DisplayLog();
+
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine("Exception : {0}", Ex.Message);
+            }
+        }
+    }
+}
